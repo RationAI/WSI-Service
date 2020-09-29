ede51b515746c27dc69a650133bfb93985d23f9e
Nick Weiss <nick.weiss@mevis.fraunhofer.de>
2020-09-29 11:38:41 +0200

    added documentation from old flask implementation

--- wsi_service/api.py
+++ wsi_service/api.py
@@ -1,11 +1,12 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, Path, Query
 from fastapi.encoders import jsonable_encoder
+from fastapi.responses import StreamingResponse
 
 from wsi_service.settings import Settings
 from wsi_service.local_mapper import LocalMapper
 from wsi_service.slide_source import SlideSource
 from wsi_service.api_utils import validate_image_request, make_image_response
-from wsi_service.models import SlideInfo
+from wsi_service.models import SlideInfo, ImageQualityQuery, ImageFormatsQuery, ImageResponses, ImageRegionResponse
 
 
 settings = Settings()
@@ -34,19 +35,25 @@ def get_slide_info(global_slide_id: str):
     return slide.get_info()
 
 
-@api.get('/slides/{global_slide_id}/thumbnail/max_size/{max_x}/{max_y}')
-def get_slide_thumbnail(global_slide_id: str, max_x: int, max_y: int, image_format: str = "jpeg", image_quality: int = 90):
-    """
-    Thumbnail of slide with given maximum size
-    """
+@api.get('/slides/{global_slide_id}/thumbnail/max_size/{max_x}/{max_y}', responses=ImageResponses, response_class=StreamingResponse)
+def get_slide_thumbnail(
+    global_slide_id: str,
+    max_x: int = Path(None, example=100, description="Maximum width of thumbnail"),
+    max_y: int = Path(None, example=100, description="Maximum height of thumbnail"),
+    image_format: str = ImageFormatsQuery,
+    image_quality: int = ImageQualityQuery):
+
     validate_image_request(image_format, image_quality)
     slide = slide_source.get_slide(global_slide_id)
     thumbnail = slide.get_thumbnail(max_x, max_y)
     return make_image_response(thumbnail, image_format, image_quality)
 
 
-@api.get('/slides/{global_slide_id}/label')
-def get_slide_label(global_slide_id: str, image_format: str = "jpeg", image_quality: int = 90):
+@api.get('/slides/{global_slide_id}/label', responses=ImageResponses, response_class=StreamingResponse)
+def get_slide_label(
+    global_slide_id: str, 
+    image_format: str = ImageFormatsQuery,
+    image_quality: int = ImageQualityQuery):
     """
     The label image of the slide
     """
@@ -56,8 +63,11 @@ def get_slide_label(global_slide_id: str, image_format: str = "jpeg", image_qual
     return make_image_response(label, image_format, image_quality)
 
 
-@api.get('/slides/{global_slide_id}/macro')
-def get_slide_macro(global_slide_id: str, image_format: str = "jpeg", image_quality: int = 90):
+@api.get('/slides/{global_slide_id}/macro', responses=ImageResponses, response_class=StreamingResponse)
+def get_slide_macro(
+    global_slide_id: str, 
+    image_format: str = ImageFormatsQuery,
+    image_quality: int = ImageQualityQuery):
     """
     The macro image of the slide
     """
@@ -68,14 +78,16 @@ def get_slide_macro(global_slide_id: str, image_format: str = "jpeg", image_qual
 
 
 @api.get(
-    '/slides/{global_slide_id}/region/level/{level}/start/{start_x}/{start_y}/size/{size_x}/{size_y}',
-    responses={
-        200: {
-            "content": {"image/*": {}}
-        }
-    }
-)
-def get_slide_region(global_slide_id: str, level: int, start_x: int, start_y: int, size_x: int, size_y: int, image_format: str = "jpeg", image_quality: int = 90):
+    '/slides/{global_slide_id}/region/level/{level}/start/{start_x}/{start_y}/size/{size_x}/{size_y}', responses=ImageRegionResponse, response_class=StreamingResponse)
+def get_slide_region(
+    global_slide_id: str, 
+    level: int = Path(None, ge=0, example=0, description="Pyramid level of region"),
+    start_x: int = Path(None, example=0, description="x component of start coordinate of requested region"),
+    start_y: int = Path(None, example=0, description="y component of start coordinate of requested region"),
+    size_x: int = Path(None, example=1024, description="Width of requested region"),
+    size_y: int = Path(None, example=1024, description="Height of requested region"),
+    image_format: str = ImageFormatsQuery,
+    image_quality: int = ImageQualityQuery):
     """
     Get region of the slide. Level 0 is highest (original) resolution. Each level has half the
     resolution and half the extent of the previous level. Coordinates are given with respect
@@ -89,8 +101,14 @@ def get_slide_region(global_slide_id: str, level: int, start_x: int, start_y: in
     return make_image_response(img, image_format, image_quality)
 
 
-@api.get('/slides/{global_slide_id}/tile/level/{level}/tile/{tile_x}/{tile_y}')
-def get_slide_tile(global_slide_id: str, level: int, tile_x: int, tile_y: int, image_format: str = "jpeg", image_quality: int = 90):
+@api.get('/slides/{global_slide_id}/tile/level/{level}/tile/{tile_x}/{tile_y}', responses=ImageResponses, response_class=StreamingResponse)
+def get_slide_tile(
+    global_slide_id: str, 
+    level: int = Path(None, ge=0, example=0, description="Pyramid level of region"), 
+    tile_x: int = Path(None, example=0, description="Request the tile_x-th tile in x dimension"),
+    tile_y: int = Path(None, example=0, description="Request the tile_y-th tile in y dimension"),
+    image_format: str = ImageFormatsQuery,
+    image_quality: int = ImageQualityQuery):
     """
     Get tile of the slide. Extent of the tile is given in slide metadata. Level 0 is highest
     (original) resolution. Each level has half the resolution and half the extent of the
--- wsi_service/models.py
+++ wsi_service/models.py
@@ -1,13 +1,43 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
+from enum import Enum
+from fastapi import Query
 
+
+# Models
 
 class Extent(BaseModel):
-    x: int
-    y: int
+    x: int = Field(..., description="Extent x in horizontal direction")
+    y: int = Field(..., description="Extent x in vertical direction")
 
 
 class SlideInfo(BaseModel):
-    extent: Extent
-    num_levels: int
-    pixel_size_nm: int
-    tile_extent: Extent
+    extent: Extent = Field(..., description="Image extent (finest level, level=0)")
+    num_levels: int = Field(..., description="Number of levels in image pyramid")
+    pixel_size_nm: int = Field(..., description="Pixel Size in nm (finest level, level=0)")
+    tile_extent: Extent = Field(..., description="Tile extent")
+
+
+# Queries
+
+ImageFormatsQuery = Query('jpeg', description="Image format (e.g. bmp, gif, jpeg, png, tiff)")
+
+
+ImageQualityQuery = Query(90, ge=0, le=100, description="Image quality (only considered for specific formats)")
+
+
+# Responses
+
+ImageResponses = {
+    200: {
+        "content": {"image/*": {}}
+    },
+    404: {
+        'detail': 'Invalid global_slide_id'
+    }
+}
+
+
+ImageRegionResponse = ImageResponses
+ImageRegionResponse[413] = {
+    'detail': 'Requested region is too large'
+}
