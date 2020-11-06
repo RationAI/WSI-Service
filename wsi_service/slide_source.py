4e006919dc349c2be3002e2d9acddbeab118a8e5
Nick Weiss <nick.weiss@mevis.fraunhofer.de>
2020-11-06 10:16:13 +0100

    Resolve "Extend Local Mapper"

--- wsi_service/slide_source.py
+++ wsi_service/slide_source.py
@@ -19,56 +19,56 @@ class SlideSource:
     def __init__(self, mapper_address, data_dir, timeout):
         atexit.register(self.close)
         self.lock = Lock()
-        self.slide_map = {}  # maps from global_slide_id to storage
+        self.slide_map = {}  # maps from slide_id to storage
         self.opened_slides = {}
         self.mapper_address = mapper_address
         self.data_dir = data_dir
         self.timeout = timeout
 
     def close(self):
-        for global_slide_id in list(self.opened_slides.keys()):
-            self.opened_slides[global_slide_id].timer.cancel()
-            self._close_slide(global_slide_id)
+        for slide_id in list(self.opened_slides.keys()):
+            self.opened_slides[slide_id].timer.cancel()
+            self._close_slide(slide_id)
 
-    def get_slide(self, global_slide_id):
+    def get_slide(self, slide_id):
         with self.lock:
-            if global_slide_id not in self.opened_slides:
+            if slide_id not in self.opened_slides:
                 try:
-                    self._map_slide(global_slide_id)
+                    self._map_slide(slide_id)
                     filepath = os.path.join(
                         self.data_dir,
-                        self.slide_map[global_slide_id]["address"],
+                        self.slide_map[slide_id]["address"],
                     )
                     slide = Slide(OpenSlide(filepath))
-                    self.opened_slides[global_slide_id] = ExpiringSlide(slide, None)
+                    self.opened_slides[slide_id] = ExpiringSlide(slide, None)
                 except KeyError:
                     raise HTTPException(status_code=404)
-            self._reset_slide_expiration(global_slide_id)
-        return self.opened_slides[global_slide_id].slide
+            self._reset_slide_expiration(slide_id)
+        return self.opened_slides[slide_id].slide
 
-    def _reset_slide_expiration(self, global_slide_id):
-        expiring_slide = self.opened_slides[global_slide_id]
+    def _reset_slide_expiration(self, slide_id):
+        expiring_slide = self.opened_slides[slide_id]
         if expiring_slide.timer is not None:
             expiring_slide.timer.cancel()
-        expiring_slide.timer = Timer(self.timeout, self._close_slide, [global_slide_id])
+        expiring_slide.timer = Timer(self.timeout, self._close_slide, [slide_id])
         expiring_slide.timer.start()
 
-    def _map_slide(self, global_slide_id):
-        if global_slide_id not in self.slide_map:
-            storage_address = self._get_slide_main_storage_address(global_slide_id)
-            self.slide_map[global_slide_id] = storage_address
+    def _map_slide(self, slide_id):
+        if slide_id not in self.slide_map:
+            storage_address = self._get_slide_main_storage_address(slide_id)
+            self.slide_map[slide_id] = storage_address
 
-    def _get_slide_main_storage_address(self, global_slide_id):
-        r = requests.get(self.mapper_address.format(global_slide_id=global_slide_id))
+    def _get_slide_main_storage_address(self, slide_id):
+        r = requests.get(self.mapper_address.format(slide_id=slide_id))
         slide = r.json()
         for storage_address in slide["storage_addresses"]:
             if storage_address["main_address"]:
                 return storage_address
         return slide["storage_addresses"][0]
 
-    def _close_slide(self, global_slide_id):
+    def _close_slide(self, slide_id):
         with self.lock:
-            if global_slide_id in self.opened_slides:
-                self.opened_slides[global_slide_id].slide.close()
-                del self.opened_slides[global_slide_id]
-                del self.slide_map[global_slide_id]
+            if slide_id in self.opened_slides:
+                self.opened_slides[slide_id].slide.close()
+                del self.opened_slides[slide_id]
+                del self.slide_map[slide_id]
--- wsi_service/tests/test_api_local_mapper.py
+++ wsi_service/tests/test_api_local_mapper.py
@@ -8,9 +8,9 @@ def test_get_cases_valid(client):
     assert response.status_code == 200
     cases = response.json()
     assert len(cases) == 9
-    assert len(cases[0].keys()) == 2
+    assert len(cases[0].keys()) == 3
     case = list(filter(lambda case: case["local_case_id"] == "Olympus", cases))[0]
-    assert case["global_case_id"] == "7a32e2c36ca756d9b7df0b627ace4c12"
+    assert case["case_id"] == "7a32e2c36ca756d9b7df0b627ace4c12"
 
 
 def test_get_available_slides_valid(client):
@@ -19,19 +19,28 @@ def test_get_available_slides_valid(client):
     slides = response.json()
     slide = list(
         filter(
-            lambda slide: slide["storage_addresses"][0]["address"].endswith(
-                "CMU-1.svs"
-            ),
+            lambda slide: slide["slide_storage"]["storage_addresses"][0][
+                "address"
+            ].endswith("CMU-1.svs"),
             slides,
         )
     )[0]
     assert len(slides) == 1
     assert len(slides[0].keys()) == 3
-    assert slide["global_slide_id"] == "f863c2ef155654b1af0387acc7ebdb60"
-    assert slide["storage_type"] == "fs"
-    assert slide["storage_addresses"][0]["address"].endswith("Aperio/CMU-1.svs")
+    assert slide["slide_id"] == "f863c2ef155654b1af0387acc7ebdb60"
+    assert slide["local_slide_id"] == "CMU-1.svs"
+    assert slide["slide_storage"]["slide_id"] == "f863c2ef155654b1af0387acc7ebdb60"
+    assert slide["slide_storage"]["storage_type"] == "fs"
+    assert slide["slide_storage"]["storage_addresses"][0]["main_address"] == True
     assert (
-        slide["storage_addresses"][0]["global_slide_id"]
+        slide["slide_storage"]["storage_addresses"][0]["storage_address_id"]
+        == "89262a18eff45876b8aa45c42c334864"
+    )
+    assert slide["slide_storage"]["storage_addresses"][0]["address"].endswith(
+        "Aperio/CMU-1.svs"
+    )
+    assert (
+        slide["slide_storage"]["storage_addresses"][0]["slide_id"]
         == "f863c2ef155654b1af0387acc7ebdb60"
     )
 
@@ -41,11 +50,20 @@ def test_get_slide_valid(client):
     assert response.status_code == 200
     slide = response.json()
     assert len(slide.keys()) == 3
-    assert slide["global_slide_id"] == "4b0ec5e0ec5e5e05ae9e500857314f20"
-    assert slide["storage_type"] == "fs"
-    assert slide["storage_addresses"][0]["address"].endswith("Generic TIFF/CMU-1.tiff")
+    assert slide["slide_id"] == "4b0ec5e0ec5e5e05ae9e500857314f20"
+    assert slide["local_slide_id"] == "CMU-1.tiff"
+    assert slide["slide_storage"]["slide_id"] == "4b0ec5e0ec5e5e05ae9e500857314f20"
+    assert slide["slide_storage"]["storage_type"] == "fs"
+    assert slide["slide_storage"]["storage_addresses"][0]["main_address"] == True
     assert (
-        slide["storage_addresses"][0]["global_slide_id"]
+        slide["slide_storage"]["storage_addresses"][0]["storage_address_id"]
+        == "ed917cbb17ab54ee84152ba30adfb4d5"
+    )
+    assert slide["slide_storage"]["storage_addresses"][0]["address"].endswith(
+        "Generic TIFF/CMU-1.tiff"
+    )
+    assert (
+        slide["slide_storage"]["storage_addresses"][0]["slide_id"]
         == "4b0ec5e0ec5e5e05ae9e500857314f20"
     )
 
@@ -71,26 +89,20 @@ def test_get_available_slides_empty_case(client_no_data):
     response = client_no_data.get("/cases/")
     assert response.status_code == 200
     cases = response.json()
-    global_case_id = cases[0]["global_case_id"]
-    response = client_no_data.get(f"/cases/{global_case_id}/slides/")
+    case_id = cases[0]["case_id"]
+    response = client_no_data.get(f"/cases/{case_id}/slides/")
     assert response.status_code == 200
     slides = response.json()
     assert len(slides) == 0
 
 
-def test_get_available_slides_invalid_global_case_id(client):
+def test_get_available_slides_invalid_case_id(client):
     response = client.get("/cases/invalid_id/slides/")
     assert response.status_code == 400
-    assert (
-        response.json()["detail"]
-        == "Case with global_case_id invalid_id does not exist"
-    )
+    assert response.json()["detail"] == "Case with case_id invalid_id does not exist"
 
 
-def test_get_slide_invalid_global_slide_id(client):
+def test_get_slide_invalid_slide_id(client):
     response = client.get("/slides/invalid_id")
     assert response.status_code == 400
-    assert (
-        response.json()["detail"]
-        == "Slide with global_slide_id invalid_id does not exist"
-    )
+    assert response.json()["detail"] == "Slide with slide_id invalid_id does not exist"
