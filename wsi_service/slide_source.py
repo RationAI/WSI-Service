0a53f48055b6fce87572625e12667d7e868c852d
Johannes Lotz <johannes@lotz.dev>
2020-12-01 15:49:09 +0100

    separating OpenSlide implementation from generic base class

--- wsi_service/slide_source.py
+++ wsi_service/slide_source.py
@@ -4,9 +4,10 @@ from threading import Lock, Timer
 
 import requests
 from fastapi import HTTPException
-from openslide import OpenSlide
 
-from wsi_service.slide import Slide
+from wsi_service.openslide_slide import OpenSlideSlide
+
+# from openslide import OpenSlide
 
 
 class ExpiringSlide:
@@ -39,7 +40,7 @@ class SlideSource:
                         self.data_dir,
                         self.slide_map[slide_id]["address"],
                     )
-                    slide = Slide(OpenSlide(filepath), slide_id)
+                    slide = OpenSlideSlide(filepath, slide_id)
                     self.opened_slides[slide_id] = ExpiringSlide(slide, None)
                 except KeyError:
                     raise HTTPException(status_code=404)
