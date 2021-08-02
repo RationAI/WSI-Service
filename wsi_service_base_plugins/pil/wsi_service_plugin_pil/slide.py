be5444c5fa3c9a3640b6787012add2de3456cf3a
Nick Weiss <nick.weiss@mevis.fraunhofer.de>
2021-08-02 14:19:19 +0200

    fixed codecheck

--- wsi_service_base_plugins/pil/wsi_service_plugin_pil/slide.py
+++ wsi_service_base_plugins/pil/wsi_service_plugin_pil/slide.py
@@ -1,14 +1,7 @@
 from fastapi import HTTPException
 from PIL import Image, UnidentifiedImageError
 
-from wsi_service.models.slide import (
-    SlideChannel,
-    SlideColor,
-    SlideExtent,
-    SlideInfo,
-    SlideLevel,
-    SlidePixelSizeNm,
-)
+from wsi_service.models.slide import SlideChannel, SlideColor, SlideExtent, SlideInfo, SlideLevel, SlidePixelSizeNm
 from wsi_service.slide import Slide as BaseSlide
 
 
@@ -17,19 +10,13 @@ class Slide(BaseSlide):
         try:
             self.slide_image = Image.open(filepath)
         except UnidentifiedImageError:
-            raise HTTPException(status_code=422, detail=f"PIL Unidentified Image Error")
+            raise HTTPException(status_code=422, detail="PIL Unidentified Image Error")
         self.slide_image = Image.open(filepath)
         width, height = self.slide_image.size
         channels = []
-        channels.append(
-            SlideChannel(id=0, name="Red", color=SlideColor(r=255, g=0, b=0, a=0))
-        )
-        channels.append(
-            SlideChannel(id=1, name="Green", color=SlideColor(r=0, g=255, b=0, a=0))
-        )
-        channels.append(
-            SlideChannel(id=2, name="Blue", color=SlideColor(r=0, g=0, b=255, a=0))
-        )
+        channels.append(SlideChannel(id=0, name="Red", color=SlideColor(r=255, g=0, b=0, a=0)))
+        channels.append(SlideChannel(id=1, name="Green", color=SlideColor(r=0, g=255, b=0, a=0)))
+        channels.append(SlideChannel(id=2, name="Blue", color=SlideColor(r=0, g=0, b=255, a=0)))
         self.slide_info = SlideInfo(
             id=slide_id,
             channels=channels,
@@ -38,11 +25,7 @@ class Slide(BaseSlide):
             num_levels=1,
             pixel_size_nm=SlidePixelSizeNm(x=0, y=0),  # pixel size unknown
             tile_extent=SlideExtent(x=width, y=height, z=1),
-            levels=[
-                SlideLevel(
-                    extent=SlideExtent(x=width, y=height, z=1), downsample_factor=1.0
-                )
-            ],
+            levels=[SlideLevel(extent=SlideExtent(x=width, y=height, z=1), downsample_factor=1.0)],
         )
 
     def close(self):
@@ -51,7 +34,7 @@ class Slide(BaseSlide):
     def get_info(self):
         return self.slide_info
 
-    def get_region(self, level, start_x, start_y, size_x, size_y, z=0):
+    def get_region(self, level, start_x, start_y, size_x, size_y, padding_color=None, z=0):
         if level != 0:
             raise HTTPException(
                 status_code=422,
@@ -65,7 +48,7 @@ class Slide(BaseSlide):
         thumbnail.thumbnail((max_x, max_y))
         return thumbnail
 
-    def get_tile(self, level, tile_x, tile_y, z=0):
+    def get_tile(self, level, tile_x, tile_y, padding_color=None, z=0):
         return self.get_region(
             level,
             tile_x * self.slide_info.tile_extent.x,
@@ -74,3 +57,15 @@ class Slide(BaseSlide):
             self.slide_info.tile_extent.y,
             z=z,
         )
+
+    def _get_associated_image(self, associated_image_name):
+        raise HTTPException(
+            status_code=404,
+            detail=f"Associated image {associated_image_name} does not exist.",
+        )
+
+    def get_label(self):
+        self._get_associated_image("label")
+
+    def get_macro(self):
+        self._get_associated_image("macro")
--- wsi_service_base_plugins/pil/wsi_service_plugin_pil/tests/test_plugin.py
+++ wsi_service_base_plugins/pil/wsi_service_plugin_pil/tests/test_plugin.py
@@ -1,8 +1,6 @@
 import pytest
 
-from wsi_service.tests.integration.plugin_example_tests.plugin_example_tests import (
-    check_get_slide_info_valid,
-)
+from wsi_service.tests.integration.plugin_example_tests.plugin_example_tests import check_get_slide_info_valid
 
 
 @pytest.mark.parametrize(
@@ -14,9 +12,5 @@ from wsi_service.tests.integration.plugin_example_tests.plugin_example_tests imp
         ("0f72fba3dc2359e18c09a1d59cd62ed0", 3, 8, 1, 0, (500, 358), 500, 358),
     ],
 )
-def test_get_slide_info_valid(
-    slide_id, channels, channel_depth, num_levels, pixel_size_nm, tile_size, x, y
-):
-    check_get_slide_info_valid(
-        slide_id, channels, channel_depth, num_levels, pixel_size_nm, tile_size, x, y
-    )
+def test_get_slide_info_valid(slide_id, channels, channel_depth, num_levels, pixel_size_nm, tile_size, x, y):
+    check_get_slide_info_valid(slide_id, channels, channel_depth, num_levels, pixel_size_nm, tile_size, x, y)
