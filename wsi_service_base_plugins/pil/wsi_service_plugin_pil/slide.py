68fffbf7ef83c23323990f6d9eb2e2f3bf2407e8
Nick Weiss <nick.weiss@mevis.fraunhofer.de>
2021-08-04 07:41:25 +0000

    removed unused imports

--- wsi_service_base_plugins/pil/wsi_service_plugin_pil/slide.py
+++ wsi_service_base_plugins/pil/wsi_service_plugin_pil/slide.py
@@ -1,7 +1,7 @@
 from fastapi import HTTPException
 from PIL import Image, UnidentifiedImageError
 
-from wsi_service.models.slide import SlideChannel, SlideColor, SlideExtent, SlideInfo, SlideLevel, SlidePixelSizeNm
+from wsi_service.models.slide import SlideExtent, SlideInfo, SlideLevel, SlidePixelSizeNm
 from wsi_service.singletons import settings
 from wsi_service.slide import Slide as BaseSlide
 from wsi_service.slide_utils import get_rgb_channel_list
