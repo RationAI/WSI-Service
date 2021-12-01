8365962d1e31c7f895a4771ccd2f58931d0dfd8d
Klaus Strohmenger <klaus.strohmenger@gmail.com>
2021-12-01 11:13:46 +0000

    bugfix loggername

--- wsi_service/singletons.py
+++ wsi_service/singletons.py
@@ -5,7 +5,7 @@ from .settings import Settings
 settings = Settings()
 
 
-logger = logging.getLogger(logger_name)
+logger = logging.getLogger("uvicorn")
 logger.setLevel(logging.INFO)
 if settings.debug:
     logger.setLevel(logging.DEBUG)
