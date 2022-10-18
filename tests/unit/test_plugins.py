ff2d0f6e755f73ddfda6e710dfa9afd6c3b0149f
Nick Weiss <nick.weiss@mevis.fraunhofer.de>
2022-10-18 10:56:59 +0200

    fixed viewer

--- tests/unit/test_plugins.py
+++ tests/unit/test_plugins.py
@@ -2,6 +2,8 @@ from wsi_service.plugins import plugins
 
 
 def test_check_available_base_plugins():
-    assert len(plugins) >= 2
-    assert "tifffile" in plugins.keys()
+    assert len(plugins) >= 4
     assert "openslide" in plugins.keys()
+    assert "pil" in plugins.keys()
+    assert "tifffile" in plugins.keys()
+    assert "wsidicom" in plugins.keys()
