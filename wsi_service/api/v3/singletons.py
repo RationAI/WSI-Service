4b0cb30dba860f6991cf29a546e49a2f9bc95653
Aiosa <469130@mail.muni.cz>
2024-08-23 10:27:47 +0200

    chore: bugfixes on integration

--- wsi_service/api/v3/singletons.py
+++ wsi_service/api/v3/singletons.py
@@ -1,7 +1,7 @@
 from ...utils.lib_utils import get_class
-from ...singletons import http_client, logger, settings
+from ...singletons import logger, settings
 from .integrations import get_api_integration
 
-api_integration = get_api_integration(settings=settings, logger=logger, http_client=http_client)
+api_integration = get_api_integration(settings=settings, logger=logger, http_client=None)
 
 MapperClass = get_class(settings.local_mode) if settings.local_mode else None
