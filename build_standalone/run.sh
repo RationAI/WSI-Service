25bbeff9e2ec0cab2bbcaa73679de4c13941e06a
Aiosa <horakj7@gmail.com>
2026-04-01 13:28:05 +0200

    chore: try to fix standalone runner to accept args to the gunicorn

--- build_standalone/run.sh
+++ build_standalone/run.sh
@@ -1,14 +1,10 @@
-#! /usr/bin/env sh
+#!/usr/bin/env sh
 set -e
-
-# Relies on pre-installation
-# or external fetching of the code library 
 
 DEFAULT_MODULE_NAME=wsi_service.app
 MODULE_NAME=${MODULE_NAME:-$DEFAULT_MODULE_NAME}
 VARIABLE_NAME=${VARIABLE_NAME:-app}
 export APP_MODULE=${APP_MODULE:-"$MODULE_NAME:$VARIABLE_NAME"}
-
 
 DEFAULT_GUNICORN_CONF=gunicorn_conf.py
 export GUNICORN_CONF=${GUNICORN_CONF:-$DEFAULT_GUNICORN_CONF}
@@ -17,13 +13,12 @@ export WORKER_CLASS=${WORKER_CLASS:-"uvicorn.workers.UvicornWorker"}
 # If there's a prestart.sh script in the /app directory or other path specified, run it before starting
 PRE_START_PATH=${PRE_START_PATH:-prestart.sh}
 echo "Checking for script in $PRE_START_PATH"
-if [ -f $PRE_START_PATH ] ; then
+if [ -f "$PRE_START_PATH" ]; then
     echo "Running script $PRE_START_PATH"
     . "$PRE_START_PATH"
-else 
+else
     echo "There is no script $PRE_START_PATH"
 fi
 
-# Start Gunicorn
-echo "gunicorn -k $WORKER_CLASS -c $GUNICORN_CONF $APP_MODULE $@"
-gunicorn -k "$WORKER_CLASS" -c "$GUNICORN_CONF" "$APP_MODULE" $@
+echo "gunicorn -k $WORKER_CLASS -c $GUNICORN_CONF $* $APP_MODULE"
+exec gunicorn -k "$WORKER_CLASS" -c "$GUNICORN_CONF" "$@" "$APP_MODULE"
